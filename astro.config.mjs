927a0f565063b5f3bd8ab305919687a75b08c8f6
canplane <canplane@gmail.com>
2026-08-23 21:09:53 +0900

    refactor: add MDX support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://canplane.com",
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
   vite: {
     plugins: [tailwindcss()],
   },
